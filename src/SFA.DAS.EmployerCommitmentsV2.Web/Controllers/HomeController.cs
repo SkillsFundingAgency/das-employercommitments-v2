377edd4e0d0f90388d13cb6f14268afc6972c975
Elliot Ritchie <elliotritchie@hotmail.com>
2019-04-29 14:26:04 +0100

    Updated homepage to require authentication

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Controllers/HomeController.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Controllers/HomeController.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace SFA.DAS.EmployerCommitmentsV2.Web.Controllers
 {
-    [AllowAnonymous]
     [Route("")]
     public class HomeController : Controller
     {
