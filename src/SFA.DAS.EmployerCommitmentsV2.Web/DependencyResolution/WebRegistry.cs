dbd5fa832a271afb1fb31ae275979b977ac5976b
chrisfoster186 <chrisfoster186@googlemail.com>
2019-06-19 13:10:24 +0100

    Adds stub api implementation and toggle

--- src/SFA.DAS.EmployerCommitmentsV2.Web/DependencyResolution/WebRegistry.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/DependencyResolution/WebRegistry.cs
@@ -1,4 +1,8 @@
-﻿using StructureMap;
+﻿using Microsoft.Extensions.Configuration;
+using SFA.DAS.EmployerAccounts.Api.Client;
+using SFA.DAS.EmployerCommitmentsV2.Services.Stubs;
+using StructureMap;
+using StructureMap.Building.Interception;
 
 namespace SFA.DAS.EmployerCommitmentsV2.Web.DependencyResolution
 {
@@ -6,6 +10,13 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.DependencyResolution
     {
         public WebRegistry()
         {
+            Toggle<IEmployerAccountsApiClient, StubEmployerAccountsApiClient>("UseStubEmployerAccountsApiClient");
+        }
+
+        private void Toggle<TPluginType, TConcreteType>(string configurationKey) where TConcreteType : TPluginType
+        {
+            For<TPluginType>().InterceptWith(new FuncInterceptor<TPluginType>((c, o) => c.GetInstance<IConfiguration>().GetValue<bool>(configurationKey) ? c.GetInstance<TConcreteType>() : o));
         }
     }
+
 }
