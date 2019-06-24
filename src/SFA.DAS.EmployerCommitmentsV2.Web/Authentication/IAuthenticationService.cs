abbc35ffb2319764cb1b2ef89c9a0ed5219cdcab
Paul Graham <paul.graham@coprime.co.uk>
2019-06-24 12:09:06 +0100

    renamed

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Authentication/IAuthenticationService.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Authentication/IAuthenticationService.cs
@@ -1,6 +1,6 @@
 ﻿namespace SFA.DAS.EmployerCommitmentsV2.Web.Authentication
 {
-    public interface IUserService
+    public interface IAuthenticationService
     {
         bool IsUserAuthenticated();
         bool TryGetUserClaimValue(string key, out string value);
