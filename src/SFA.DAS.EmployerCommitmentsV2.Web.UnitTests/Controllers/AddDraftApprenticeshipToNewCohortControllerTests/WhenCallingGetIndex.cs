257847d28c5dfe978f9a9c50ac83d5cfd87a10e0
Scott Wakefield <scott@expertcc.co.uk>
2019-07-09 15:08:56 +0100

    CV-191 added action and test for controller.

---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web.UnitTests/Controllers/AddDraftApprenticeshipToNewCohortControllerTests/WhenCallingGetIndex.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using SFA.DAS.EmployerCommitmentsV2.Web.Controllers;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Controllers.AddDraftApprenticeshipToNewCohortControllerTests
+{
+    public class WhenCallingGetIndex
+    {
+        [Test, MoqAutoData]
+        public void Then_Returns_View(
+            string accountId,
+            AddDraftApprenticeshipToNewCohortController controller)
+        {
+            var result = controller.Index(accountId) as ViewResult;
+
+            result.ViewName.Should().BeNull();
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Controllers/AddDraftApprenticeshipToNewCohortController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.Controllers
+{
+    [Route("{accountId}/unapproved/add")]
+    public class AddDraftApprenticeshipToNewCohortController : Controller
+    {
+        public IActionResult Index(string accountId)
+        {
+            return View(accountId);
+        }
+    }
+}
