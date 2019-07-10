2011552991928a62bc33dccc840505d18df1705c
Scott Wakefield <scott@expertcc.co.uk>
2019-07-10 17:04:28 +0100

    CV-191 made it easier to get the route data into the tag helper.

---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web.UnitTests/Models/AddDraftApprenticeshipToNewCohort/WhenCastingRouteModelToDictionary.cs
@@ -0,0 +1,92 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerCommitmentsV2.Web.Models.AddDraftApprenticeshipToNewCohort;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Models.AddDraftApprenticeshipToNewCohort
+{
+    public class WhenCastingRouteModelToDictionary
+    {
+        [Test, AutoData]
+        public void Then_Adds_AccountId_To_Dictionary(
+            RouteModel routeModel)
+        {
+            var dictionary = routeModel.ToDictionary();
+
+            dictionary.Should().ContainKey(nameof(RouteModel.AccountId))
+                .WhichValue.Should().Be(routeModel.AccountId);
+        }
+
+        [Test, AutoData]
+        public void Then_Adds_ReservationId_To_Dictionary(
+            RouteModel routeModel)
+        {
+            var dictionary = routeModel.ToDictionary();
+
+            dictionary.Should().ContainKey(nameof(RouteModel.ReservationId))
+                .WhichValue.Should().Be(routeModel.ReservationId);
+        }
+
+        [Test, AutoData]
+        public void Then_Adds_AleId_To_Dictionary(
+            RouteModel routeModel)
+        {
+            var dictionary = routeModel.ToDictionary();
+
+            dictionary.Should().ContainKey(nameof(RouteModel.EmployerAccountLegalEntityPublicHashedId))
+                .WhichValue.Should().Be(routeModel.EmployerAccountLegalEntityPublicHashedId);
+        }
+
+        [Test, AutoData]
+        public void Then_Adds_StartMonthYear_To_Dictionary(
+            RouteModel routeModel)
+        {
+            var dictionary = routeModel.ToDictionary();
+
+            dictionary.Should().ContainKey(nameof(RouteModel.StartMonthYear))
+                .WhichValue.Should().Be(routeModel.StartMonthYear);
+        }
+
+        [Test, AutoData]
+        public void Then_Adds_CourseCode_To_Dictionary(
+            RouteModel routeModel)
+        {
+            var dictionary = routeModel.ToDictionary();
+
+            dictionary.Should().ContainKey(nameof(RouteModel.CourseCode))
+                .WhichValue.Should().Be(routeModel.CourseCode);
+        }
+
+        // missing value cases
+
+        [Test, AutoData]
+        public void And_No_ReservationId_Then_Not_Add_ReservationId_To_Dictionary(
+            RouteModel routeModel)
+        {
+            routeModel.ReservationId = null;
+            var dictionary = routeModel.ToDictionary();
+
+            dictionary.Should().NotContainKey(nameof(RouteModel.ReservationId));
+        }
+
+        [Test, AutoData]
+        public void And_No_StartMonthYear_Then_Not_Add_StartMonthYear_To_Dictionary(
+            RouteModel routeModel)
+        {
+            routeModel.StartMonthYear = null;
+            var dictionary = routeModel.ToDictionary();
+
+            dictionary.Should().NotContainKey(nameof(RouteModel.StartMonthYear));
+        }
+
+        [Test, AutoData]
+        public void And_No_CourseCode_Then_Not_Add_CourseCode_To_Dictionary(
+            RouteModel routeModel)
+        {
+            routeModel.CourseCode = null;
+            var dictionary = routeModel.ToDictionary();
+
+            dictionary.Should().NotContainKey(nameof(RouteModel.CourseCode));
+        }
+    }
+}
--- src/SFA.DAS.EmployerCommitmentsV2.Web/Models/AddDraftApprenticeshipToNewCohort/RouteModel.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Models/AddDraftApprenticeshipToNewCohort/RouteModel.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SFA.DAS.EmployerCommitmentsV2.Web.Models.AddDraftApprenticeshipToNewCohort
 {
@@ -14,5 +15,23 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.Models.AddDraftApprenticeshipToNewCo
         public string StartMonthYear { get; set; }
         [FromQuery]
         public string CourseCode { get; set; }
+
+        public Dictionary<string, string> ToDictionary()
+        {
+            var dictionary = new Dictionary<string, string>
+            {
+                {nameof(AccountId), AccountId },
+                {nameof(EmployerAccountLegalEntityPublicHashedId), EmployerAccountLegalEntityPublicHashedId }
+            };
+            
+            if (!string.IsNullOrWhiteSpace(ReservationId))
+                dictionary.Add(nameof(ReservationId), ReservationId);
+            if (!string.IsNullOrWhiteSpace(StartMonthYear))
+                dictionary.Add(nameof(StartMonthYear), StartMonthYear);
+            if (!string.IsNullOrWhiteSpace(CourseCode))
+                dictionary.Add(nameof(CourseCode), CourseCode);
+
+            return dictionary;
+        }
     }
 }
