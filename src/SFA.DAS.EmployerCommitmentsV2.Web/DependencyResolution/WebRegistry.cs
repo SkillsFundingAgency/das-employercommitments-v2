4b3db75928b116c40c70fefa818ebdeaa8778b14
Scott Wakefield <scott@expertcc.co.uk>
2019-07-11 14:37:13 +0100

    CV-191 added ioc config for url helper.

--- src/SFA.DAS.EmployerCommitmentsV2.Web/DependencyResolution/WebRegistry.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/DependencyResolution/WebRegistry.cs
@@ -2,6 +2,7 @@
 using SFA.DAS.EmployerCommitmentsV2.Configuration;
 using SFA.DAS.EmployerCommitmentsV2.Services.Stubs;
 using SFA.DAS.EmployerCommitmentsV2.Web.Authentication;
+using SFA.DAS.EmployerUrlHelper;
 using StructureMap;
 using StructureMap.Building.Interception;
 
@@ -15,6 +16,7 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.DependencyResolution
             For<StubEmployerAccountsApiClient>().Use<StubEmployerAccountsApiClient>().Singleton();
             For<IEmployerAccountsApiClient>().InterceptWith(new FuncInterceptor<IEmployerAccountsApiClient>(
                 (c, o) => c.GetInstance<EmployerCommitmentsV2Configuration>().UseStubEmployerAccountsApiClient ? c.GetInstance<StubEmployerAccountsApiClient>() : o));
+            For<ILinkGenerator>().Use<LinkGenerator>();
         }
     }
 }
