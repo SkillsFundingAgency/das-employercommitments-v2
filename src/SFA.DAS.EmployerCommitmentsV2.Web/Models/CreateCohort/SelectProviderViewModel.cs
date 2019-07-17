ed18abb2c89ff796316010d827a387683c74dad2
Akshay Chugh <akshay.chugh30@outlook.com>
2019-07-17 11:28:05 +0100

    Added ViewModel

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Models/CreateCohort/SelectProviderViewModel.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Models/CreateCohort/SelectProviderViewModel.cs
@@ -2,13 +2,14 @@
 {
     public class SelectProviderViewModel
     {
+        public string AccountHashedId { get; set; }
         public string AccountId { get; set; }
-       
+
         public string ReservationId { get; set; }
-       
+
         public string EmployerAccountLegalEntityPublicHashedId { get; set; }
-       
-       public string StartMonthYear { get; set; }
+
+        public string StartMonthYear { get; set; }
 
         public string CourseCode { get; set; }
 
