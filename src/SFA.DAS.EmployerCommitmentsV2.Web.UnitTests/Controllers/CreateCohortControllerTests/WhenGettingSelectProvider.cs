66f91807ab42650a7be41d3381b868b03c3d2600
Akshay Chugh <akshay.chugh30@outlook.com>
2019-07-17 11:41:42 +0100

    Added Unit Test for SelectProvider GET

---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web.UnitTests/Controllers/CreateCohortControllerTests/WhenGettingSelectProvider.cs
@@ -0,0 +1,41 @@
+﻿using AutoFixture.NUnit3;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Commitments.Shared.Interfaces;
+using SFA.DAS.EmployerCommitmentsV2.Web.Controllers;
+using SFA.DAS.EmployerCommitmentsV2.Web.Models.CreateCohort;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Controllers.CreateCohortControllerTests
+{
+    [TestFixture]
+    public class WhenGettingSelectProvider
+    {
+        [Test, MoqAutoData]
+        public void ThenMapsTheRequestToViewModel(
+            SelectProviderRequest request,
+            SelectProviderViewModel viewModel,
+            [Frozen] Mock<IMapper<SelectProviderRequest, SelectProviderViewModel>> mockMapper,
+            CreateCohortController controller)
+        {
+            mockMapper.Setup(x => x.Map(request)).Returns(viewModel);
+
+            controller.SelectProvider(request);
+
+            mockMapper.Verify(x => x.Map(It.IsAny<SelectProviderRequest>()), Times.Once);
+        }
+
+        [Test, MoqAutoData]
+        public void ThenReturnsView(SelectProviderRequest request, CreateCohortController controller)
+        {
+            var viewName = "SelectProvider";
+
+            var result = controller.SelectProvider(request) as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.AreEqual(viewName, result.ViewName);
+
+        }
+    }
+}
