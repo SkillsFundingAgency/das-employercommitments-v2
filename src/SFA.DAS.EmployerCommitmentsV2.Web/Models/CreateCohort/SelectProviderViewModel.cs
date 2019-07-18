79bae92e44f6d4fb166935ae72005b54073c4d36
Akshay Chugh <akshay.chugh30@outlook.com>
2019-07-18 11:47:55 +0100

    Adjusted SelectProviderViewModel

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Models/CreateCohort/SelectProviderViewModel.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Models/CreateCohort/SelectProviderViewModel.cs
@@ -1,18 +1,14 @@
-﻿namespace SFA.DAS.EmployerCommitmentsV2.Web.Models.CreateCohort
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.Models.CreateCohort
 {
     public class SelectProviderViewModel
     {
         public string AccountHashedId { get; set; }
-        public string AccountId { get; set; }
-
         public string ReservationId { get; set; }
-
         public string EmployerAccountLegalEntityPublicHashedId { get; set; }
-
         public string StartMonthYear { get; set; }
-
         public string CourseCode { get; set; }
-
         public long ProviderId { get; set; }
     }
 }
