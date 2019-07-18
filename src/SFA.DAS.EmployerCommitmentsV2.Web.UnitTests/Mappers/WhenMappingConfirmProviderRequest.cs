f4977548ae095572e0da2d97202c2180d62b2b14
Akshay Chugh <akshay.chugh30@outlook.com>
2019-07-18 11:42:25 +0100

    Added ConfirmProviderRequestMapper

---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web.UnitTests/Mappers/WhenMappingConfirmProviderRequest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using SFA.DAS.EmployerCommitmentsV2.Web.Mappers;
+using SFA.DAS.EmployerCommitmentsV2.Web.Models.CreateCohort;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Mappers
+{
+    [TestFixture]
+    public class WhenMappingConfirmProviderRequest
+    {
+        [Test, MoqAutoData]
+        public void ThenMapsReservationId(
+            SelectProviderViewModel viewModel,
+            ConfirmProviderRequestMapper mapper)
+        {
+            var result = mapper.Map(viewModel);
+
+            Assert.AreEqual(viewModel.ReservationId,result.ReservationId);
+        }
+
+        [Test, MoqAutoData]
+        public void ThenMapsCourseCode(
+            SelectProviderViewModel viewModel,
+            ConfirmProviderRequestMapper mapper)
+        {
+            var result = mapper.Map(viewModel);
+
+            Assert.AreEqual(viewModel.CourseCode, result.CourseCode);
+        }
+
+        [Test, MoqAutoData]
+        public void ThenMapsAccountHashedId(
+            SelectProviderViewModel viewModel,
+            ConfirmProviderRequestMapper mapper)
+        {
+            var result = mapper.Map(viewModel);
+
+            Assert.AreEqual(viewModel.AccountHashedId, result.AccountHashedId);
+        }
+
+        [Test, MoqAutoData]
+        public void ThenMapsEmployerAccountLegalEntityPublicHashedId(
+            SelectProviderViewModel viewModel,
+            ConfirmProviderRequestMapper mapper)
+        {
+            var result = mapper.Map(viewModel);
+
+            Assert.AreEqual(viewModel.EmployerAccountLegalEntityPublicHashedId, result.EmployerAccountLegalEntityPublicHashedId);
+        }
+
+        [Test, MoqAutoData]
+        public void ThenMapsStartMonthYear(
+            SelectProviderViewModel viewModel,
+            ConfirmProviderRequestMapper mapper)
+        {
+            var result = mapper.Map(viewModel);
+
+            Assert.AreEqual(viewModel.StartMonthYear, result.StartMonthYear);
+        }
+
+        [Test, MoqAutoData]
+        public void ThenMapsProviderId(
+            SelectProviderViewModel viewModel,
+            ConfirmProviderRequestMapper mapper)
+        {
+            var result = mapper.Map(viewModel);
+
+            Assert.AreEqual(viewModel.ProviderId, result.ProviderId);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Mappers/ConfirmProviderRequestMapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Formatters.Xml;
+using SFA.DAS.Commitments.Shared.Interfaces;
+using SFA.DAS.EmployerCommitmentsV2.Web.Models.CreateCohort;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.Mappers
+{
+    public class ConfirmProviderRequestMapper : IMapper<SelectProviderViewModel, ConfirmProviderRequest>
+    {
+        public ConfirmProviderRequest Map(SelectProviderViewModel source)
+        {
+            return new ConfirmProviderRequest
+            {
+                AccountHashedId = source.AccountHashedId,
+                CourseCode = source.CourseCode,
+                EmployerAccountLegalEntityPublicHashedId = source.EmployerAccountLegalEntityPublicHashedId,
+                ProviderId = source.ProviderId,
+                ReservationId = source.ReservationId,
+                StartMonthYear = source.StartMonthYear
+            };
+        }
+    }
+}
