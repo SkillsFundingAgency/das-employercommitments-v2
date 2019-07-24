9e3f32fac3f4c94ccfd316f1825e26bc68192518
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-07-24 11:35:44 +0100

    Remove not needed and duplicated test file

--- src/SFA.DAS.EmployerCommitmentsV2/Application/Queries/Providers/GetProviderResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SFA.DAS.EmployerCommitmentsV2.Application.Queries.Providers
-{
-    public class GetProviderResponse
-    {
-        public long ProviderId { get; set; }
-        public string ProviderName { get; set; }
-    }
-}
