00de93631f6e10d49725cbaf2fd5c36ea13c5f3a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-07-25 10:22:49 +0100

    Add confirm provider view model validator

---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Validators/ConfirmProviderViewModelValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using SFA.DAS.EmployerCommitmentsV2.Web.Models.CreateCohort;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.Validators
+{
+    public class ConfirmProviderViewModelValidator : AbstractValidator<ConfirmProviderViewModel>
+    {
+        public ConfirmProviderViewModelValidator()
+        {
+            RuleFor(x => x.UseThisProvider).Must(x => x.HasValue).WithMessage("Select a training provider");
+        }
+    }
+}
