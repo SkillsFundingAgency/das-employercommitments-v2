4cbf8c75be741a974922d0663d383967a3b6210c
chrisfoster186 <chrisfoster186@googlemail.com>
2019-08-21 12:58:19 +0100

    Removes data annotation from index request

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Cohort/IndexRequest.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Cohort/IndexRequest.cs
@@ -6,10 +6,8 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.Models.Cohort
     public class IndexRequest
     {
         [FromRoute]
-        [Required]
         public string AccountHashedId { get; set; }
         public string ReservationId { get; set; }
-        [Required]
         public string AccountLegalEntityHashedId { get; set; }
 
         public string StartMonthYear { get; set; }
---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Validators/IndexRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using SFA.DAS.EmployerCommitmentsV2.Web.Models.Cohort;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.Validators
+{
+    public class IndexRequestValidator : AbstractValidator<IndexRequest>
+    {
+        public IndexRequestValidator()
+        {
+            RuleFor(x => x.AccountHashedId).NotEmpty();
+            RuleFor(x => x.AccountLegalEntityHashedId).NotEmpty();
+        }
+    }
+}
