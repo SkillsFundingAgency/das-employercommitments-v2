042f9bbe50107a4fba30e9eef3be379530f30753
chrisfoster186 <chrisfoster186@googlemail.com>
2019-09-03 14:42:08 +0100

    Basic mapper implementation for Edit vm

---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web.UnitTests/Mappers/DraftApprenticeship/EditDraftApprenticeshipViewModelMapperTests.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Api.Client;
+using SFA.DAS.CommitmentsV2.Api.Types.Responses;
+using SFA.DAS.EmployerCommitmentsV2.Web.Mappers.DraftApprenticeship;
+using SFA.DAS.EmployerCommitmentsV2.Web.Models.DraftApprenticeship;
+using SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Extensions;
+using SFA.DAS.Encoding;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Mappers.DraftApprenticeship
+{
+    [TestFixture]
+    public class EditDraftApprenticeshipViewModelMapperTests
+    {
+        private EditDraftApprenticeshipViewModelMapper _mapper;
+        private EditDraftApprenticeshipRequest _request;
+        private EditDraftApprenticeshipViewModel _result;
+
+        private Mock<ICommitmentsApiClient> _commitmentsApiClient;
+        private GetDraftApprenticeshipResponse _draftApprenticeshipResponse;
+        private Mock<IEncodingService> _encodingService;
+        private string _encodedApprenticeshipId;
+        private string _cohortReference;
+        private GetCohortResponse _cohortDetails;
+
+        [SetUp]
+        public async Task Arrange()
+        {
+            var autoFixture = new Fixture();
+
+            _encodedApprenticeshipId = autoFixture.Create<string>();
+            _cohortReference = autoFixture.Create<string>();
+
+            _encodingService = new Mock<IEncodingService>();
+            _encodingService
+                .Setup(x => x.Encode(It.IsAny<long>(), It.Is<EncodingType>(e => e == EncodingType.ApprenticeshipId)))
+                .Returns(_encodedApprenticeshipId);
+            _encodingService
+                .Setup(x => x.Encode(It.IsAny<long>(), It.Is<EncodingType>(e => e == EncodingType.CohortReference)))
+                .Returns(_cohortReference);
+
+            _draftApprenticeshipResponse = autoFixture.Create<GetDraftApprenticeshipResponse>();
+            _commitmentsApiClient = new Mock<ICommitmentsApiClient>();
+            _commitmentsApiClient.Setup(x =>
+                    x.GetDraftApprenticeship(It.IsAny<long>(), It.IsAny<long>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(_draftApprenticeshipResponse);
+
+            _cohortDetails = autoFixture.Create<GetCohortResponse>();
+            _commitmentsApiClient.Setup(x => x.GetCohort(It.IsAny<long>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(_cohortDetails);
+
+            _request = autoFixture.Create<EditDraftApprenticeshipRequest>();
+            _mapper = new EditDraftApprenticeshipViewModelMapper(_commitmentsApiClient.Object, _encodingService.Object);
+
+            _result = await _mapper.Map(TestHelper.Clone(_request));
+        }
+
+        [Test]
+        public void DraftApprenticeshipIdIsMappedCorrectly()
+        {
+            Assert.AreEqual(_draftApprenticeshipResponse.Id, _result.DraftApprenticeshipId);
+        }
+
+        [Test]
+        public void DraftApprenticeshipHashedIdIsMappedCorrectly()
+        {
+            Assert.AreEqual(_encodedApprenticeshipId, _result.DraftApprenticeshipHashedId);
+        }
+
+        [Test]
+        public void CohortIdIsMappedCorrectly()
+        {
+            Assert.AreEqual(_request.CohortId, _result.CohortId);
+        }
+
+        [Test]
+        public void CohortReferenceIsMappedCorrectly()
+        {
+            Assert.AreEqual(_cohortReference, _result.CohortReference);
+        }
+
+        [Test]
+        public void ReservationIdIsMappedCorrectly()
+        {
+            Assert.AreEqual(_draftApprenticeshipResponse.ReservationId, _result.ReservationId);
+        }
+
+        [Test]
+        public void FirstNameIsMappedCorrectly()
+        {
+            Assert.AreEqual(_draftApprenticeshipResponse.FirstName, _result.FirstName);
+        }
+
+        [Test]
+        public void LastNameIsMappedCorrectly()
+        {
+            Assert.AreEqual(_draftApprenticeshipResponse.LastName, _result.LastName);
+        }
+
+        [Test]
+        public void DateOfBirthIsMappedCorrectly()
+        {
+            Assert.AreEqual(_draftApprenticeshipResponse.DateOfBirth, _result.DateOfBirth.Date);
+        }
+
+        [Test]
+        public void UniqueLearnerNumberIsMappedCorrectly()
+        {
+            Assert.AreEqual(_draftApprenticeshipResponse.Uln, _result.Uln);
+        }
+
+        [Test]
+        public void CourseCodeIsMappedCorrectly()
+        {
+            Assert.AreEqual(_draftApprenticeshipResponse.CourseCode, _result.CourseCode);
+        }
+
+        [Test]
+        public void CostIsMappedCorrectly()
+        {
+            Assert.AreEqual(_draftApprenticeshipResponse.Cost, _result.Cost);
+        }
+
+        [Test]
+        public void StartDateIsMappedCorrectly()
+        {
+            Assert.AreEqual(_draftApprenticeshipResponse.StartDate.ToMonthYearString(), _result.StartDate.Date.ToMonthYearString());
+        }
+
+        [Test]
+        public void EndDateIsMappedCorrectly()
+        {
+            Assert.AreEqual(_draftApprenticeshipResponse.EndDate.ToMonthYearString(), _result.EndDate.Date.ToMonthYearString());
+        }
+
+        [Test]
+        public void OriginatorReferenceIsMappedCorrectly()
+        {
+            Assert.AreEqual(_draftApprenticeshipResponse.Reference, _result.Reference);
+        }
+
+        [Test]
+        public void AccountHashedIdIsMappedCorrectly()
+        {
+            Assert.AreEqual(_request.AccountHashedId, _result.AccountHashedId);
+        }
+
+        [Test]
+        public void ProviderNameIsMappedCorrectly()
+        {
+            Assert.AreEqual(_cohortDetails.ProviderName, _result.ProviderName);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Mappers/DraftApprenticeship/EditDraftApprenticeshipViewModelMapper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using SFA.DAS.Commitments.Shared.Interfaces;
+using SFA.DAS.CommitmentsV2.Api.Client;
+using SFA.DAS.EmployerCommitmentsV2.Web.Models.DraftApprenticeship;
+using SFA.DAS.Encoding;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.Mappers.DraftApprenticeship
+{
+    public class EditDraftApprenticeshipViewModelMapper : IMapper<EditDraftApprenticeshipRequest, EditDraftApprenticeshipViewModel>
+    {
+        private readonly ICommitmentsApiClient _commitmentsApiClient;
+        private readonly IEncodingService _encodingService;
+
+        public EditDraftApprenticeshipViewModelMapper(ICommitmentsApiClient commitmentsApiClient, IEncodingService encodingService)
+        {
+            _commitmentsApiClient = commitmentsApiClient;
+            _encodingService = encodingService;
+        }
+
+        public async Task<EditDraftApprenticeshipViewModel> Map(EditDraftApprenticeshipRequest source)
+        {
+            var draftApprenticeship = await _commitmentsApiClient.GetDraftApprenticeship(source.CohortId, source.DraftApprenticeshipId);
+            var cohort = await _commitmentsApiClient.GetCohort(source.CohortId);
+
+            return new EditDraftApprenticeshipViewModel(draftApprenticeship.DateOfBirth, draftApprenticeship.StartDate, draftApprenticeship.EndDate)
+            {
+                DraftApprenticeshipId = draftApprenticeship.Id,
+                DraftApprenticeshipHashedId = _encodingService.Encode(draftApprenticeship.Id, EncodingType.ApprenticeshipId),
+                CohortId = source.CohortId,
+                CohortReference = _encodingService.Encode(source.CohortId, EncodingType.CohortReference),
+                ReservationId = draftApprenticeship.ReservationId,
+                FirstName = draftApprenticeship.FirstName,
+                LastName = draftApprenticeship.LastName,
+                Uln = draftApprenticeship.Uln,
+                CourseCode = draftApprenticeship.CourseCode,
+                Cost = draftApprenticeship.Cost,
+                Reference = draftApprenticeship.Reference,
+                AccountHashedId = source.AccountHashedId,
+                ProviderName = cohort.ProviderName
+            };
+        }
+    }
+}
