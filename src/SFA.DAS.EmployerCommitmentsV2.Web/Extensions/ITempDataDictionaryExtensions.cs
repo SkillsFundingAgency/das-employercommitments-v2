b497182a69a0b95e358e863f2b671ff0631cdeb3
chrisfoster186 <chrisfoster186@googlemail.com>
2019-09-26 15:02:39 +0100

    POC

---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Extensions/ITempDataDictionaryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.Extensions
+{
+    public static class ITempDataDictionaryExtensions
+    {
+        public static void AddFlashMessage(this ITempDataDictionary tempData, string message, FlashMessageLevel level)
+        {
+            tempData["FlashMessage"] = message;
+            tempData["FlashMessageLevel"] = level;
+        }
+    }
+    public enum FlashMessageLevel
+    {
+        Info,
+        Warning,
+        Success
+    }
+}
