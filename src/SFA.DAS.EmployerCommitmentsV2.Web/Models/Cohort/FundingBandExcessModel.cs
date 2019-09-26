f4c6aa0ca6a055a0c27b141fd6f98d50c3615f01
Paul Graham <paul.graham@coprime.co.uk>
2019-09-26 15:26:40 +0100

    removed un-used refs

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Cohort/FundingBandExcessModel.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Cohort/FundingBandExcessModel.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Globalization;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace SFA.DAS.EmployerCommitmentsV2.Web.Models.Cohort
