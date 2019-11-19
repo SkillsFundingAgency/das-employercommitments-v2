91c0538516f1d5bafd7760be0de8dbe64cbb8ac9
Akshay Chugh <akshay.chugh30@outlook.com>
2019-11-19 11:39:35 +0000

    CV-473 DraftRequestValidator - unit tests & code

---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web.UnitTests/Validators/DraftRequestValidatorTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq.Expressions;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+using SFA.DAS.EmployerCommitmentsV2.Web.Models.Cohort;
+using SFA.DAS.EmployerCommitmentsV2.Web.Validators;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Validators
+{
+    [TestFixture]
+    public class DraftRequestValidatorTests
+    {
+
+        [TestCase(1, true)]
+        [TestCase(default(long), false)]
+        public void ThenValidatesAccountId(long accountId, bool expectedValid)
+        {
+            var request = new DraftRequest {AccountId = accountId};
+
+            AssertValidationResult(x => x.AccountId, request, expectedValid);
+        }
+
+        [TestCase("", false)]
+        [TestCase(" ", false)]
+        [TestCase("testString", true)]
+        [TestCase(null, false)]
+        public void ThenValidatesAccountHashedId(string accountHashedId, bool expectedValid)
+        {
+            var request = new DraftRequest { AccountHashedId = accountHashedId};
+
+            AssertValidationResult(x => x.AccountHashedId, request, expectedValid);
+        }
+
+
+        private void AssertValidationResult<T>(Expression<Func<DraftRequest, T>> property, DraftRequest instance, bool expectedValid)
+        {
+            var validator = new DraftRequestValidator();
+
+            if (expectedValid)
+            {
+                validator.ShouldNotHaveValidationErrorFor(property, instance);
+            }
+            else
+            {
+                validator.ShouldHaveValidationErrorFor(property, instance);
+            }
+        }
+    }
+}
