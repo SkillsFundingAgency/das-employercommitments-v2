f06fb8058ec607f7b8fc7d9918c2b96501dca07a
Akshay Chugh <akshay.chugh30@outlook.com>
2020-06-11 18:31:39 +0100

    CV-771 Org added to GaData model

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Shared/GaData.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Shared/GaData.cs
@@ -8,6 +8,7 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.Models.Shared
         public string UserId { get; set; }
         public string Vpv { get; set; }
         public string Acc { get; set; }
+        public string Org { get; set; }
         public IDictionary<string, string> Extras { get; set; } = new Dictionary<string, string>();
     }
 }
