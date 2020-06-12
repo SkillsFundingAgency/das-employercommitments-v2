0baa012c7d2f8802d9e2c74c278e45ff3605d51b
Akshay Chugh <akshay.chugh30@outlook.com>
2020-06-12 16:35:36 +0100

    CV-771 Apprentice.cshtml org tests + implementation

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Cohort/ApprenticeViewModel.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Cohort/ApprenticeViewModel.cs
@@ -10,6 +10,7 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.Models.Cohort
 
         public long AccountLegalEntityId { get; set; }
         public string AccountLegalEntityHashedId { get; set; }
+        public string AccountLegalEntityName { get; set; }
         public string TransferSenderId { get; set; }
         public long? DecodedTransferSenderId { get; set; }
         public Origin Origin { get; set; }
