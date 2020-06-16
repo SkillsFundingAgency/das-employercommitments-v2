1fc4ef4b8c7406fc7a8746b8005f01eea4091e17
Shahzad <shahzad.khan@hkf-technologies.co.uk>
2020-06-16 11:55:09 +0100

    Nlog fix

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Program.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using NLog.Web;
 using SFA.DAS.EmployerCommitmentsV2.Web.Startup;
 using StructureMap.AspNetCore;
+using System;
 
 namespace SFA.DAS.EmployerCommitmentsV2.Web
 {
@@ -10,6 +11,10 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web
     {
         public static void Main(string[] args)
         {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var logger = NLogBuilder.ConfigureNLog(environment == "Development" ? "nlog.Development.config" : "nlog.config").GetCurrentClassLogger();
+            logger.Info("Starting up host");
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
