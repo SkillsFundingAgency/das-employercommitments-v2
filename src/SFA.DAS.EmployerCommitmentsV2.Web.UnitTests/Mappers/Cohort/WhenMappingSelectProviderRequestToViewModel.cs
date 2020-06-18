1750cb4a396c80fe0548af15bcabed2faf324ff2
Akshay Chugh <akshay.chugh30@outlook.com>
2020-06-18 20:20:45 +0100

    Tech review unit test change

--- src/SFA.DAS.EmployerCommitmentsV2.Web.UnitTests/Mappers/Cohort/WhenMappingSelectProviderRequestToViewModel.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web.UnitTests/Mappers/Cohort/WhenMappingSelectProviderRequestToViewModel.cs
@@ -23,10 +23,10 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Mappers.Cohort
         public void Setup()
         {
             var autoFixture = new Fixture();
-            _request = autoFixture.Freeze<SelectProviderRequest>();
-            _commitmentsApiClientResponse = autoFixture.Freeze<AccountLegalEntityResponse>();
+            _request = autoFixture.Create<SelectProviderRequest>();
+            _commitmentsApiClientResponse = autoFixture.Create<AccountLegalEntityResponse>();
 
-            _commitmentsApiClientMock = autoFixture.Freeze<Mock<ICommitmentsApiClient>>();
+            _commitmentsApiClientMock = new Mock<ICommitmentsApiClient>();
             _commitmentsApiClientMock
                 .Setup(x => x.GetAccountLegalEntity(_request.AccountLegalEntityId, CancellationToken.None))
                 .ReturnsAsync(_commitmentsApiClientResponse);
@@ -34,7 +34,7 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Mappers.Cohort
             _mapper = new SelectProviderViewModelMapper(_commitmentsApiClientMock.Object);
         }
 
-        [Test, MoqAutoData]
+        [Test]
         public async Task ThenMapsReservationId()
         {
             var result = await _mapper.Map(_request);
@@ -42,7 +42,7 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Mappers.Cohort
             Assert.AreEqual(_request.ReservationId, result.ReservationId);
         }
 
-        [Test, MoqAutoData]
+        [Test]
         public async Task ThenMapsAccountHashedId()
         {
             var result = await _mapper.Map(_request);
@@ -50,7 +50,7 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Mappers.Cohort
             Assert.AreEqual(_request.AccountHashedId, result.AccountHashedId);
         }
 
-        [Test, MoqAutoData]
+        [Test]
         public async Task ThenMapsLegalEntityName()
         {
             var result = await _mapper.Map(_request);
@@ -58,7 +58,7 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Mappers.Cohort
             Assert.AreEqual(_commitmentsApiClientResponse.LegalEntityName, result.LegalEntityName);
         }
 
-        [Test, MoqAutoData]
+        [Test]
         public async Task ThenMapsCourseCode()
         {
             var result = await _mapper.Map(_request);
@@ -66,7 +66,7 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Mappers.Cohort
             Assert.AreEqual(_request.CourseCode, result.CourseCode);
         }
 
-        [Test, MoqAutoData]
+        [Test]
         public async Task ThenMapsStartMonthYear()
         {
             var result = await _mapper.Map(_request);
@@ -74,7 +74,7 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Mappers.Cohort
             Assert.AreEqual(_request.StartMonthYear, result.StartMonthYear);
         }
 
-        [Test, MoqAutoData]
+        [Test]
         public async Task ThenMapsEmployerAccountLegalEntityPublicHashedId()
         {
             var result = await _mapper.Map(_request);
@@ -82,7 +82,7 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Mappers.Cohort
             Assert.AreEqual(_request.AccountLegalEntityHashedId, result.AccountLegalEntityHashedId);
         }
 
-        [Test, MoqAutoData]
+        [Test]
         public async Task ThenMapsTransferSenderId()
         {
             var result = await _mapper.Map(_request);
@@ -90,7 +90,7 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Mappers.Cohort
             Assert.AreEqual(_request.TransferSenderId, result.TransferSenderId);
         }
 
-        [Test, MoqAutoData]
+        [Test]
         public async Task ThenMapsOrigin()
         {
             var result = await _mapper.Map(_request);
