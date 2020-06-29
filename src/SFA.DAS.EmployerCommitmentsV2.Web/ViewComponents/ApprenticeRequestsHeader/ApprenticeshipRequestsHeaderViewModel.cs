4d89d4d1a8b2fb641d242c5629d9e75f411c23db
Ben Stone <ben1.stone@education.gov.uk>
2020-06-29 09:54:27 +0100

    header component in progress

---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/ViewComponents/ApprenticeRequestsHeader/ApprenticeshipRequestsHeaderViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.ViewComponents.ApprenticeRequestsHeader
+{
+    public class ApprenticeshipRequestsHeaderViewModel
+    {
+    }
+}
