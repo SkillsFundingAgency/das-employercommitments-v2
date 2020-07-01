10e6a97b664c4acb528be310cf9cac404fbbab2c
Paul Howes <paul.howes@cognetics.net>
2020-07-01 13:00:53 +0100

    Updated Review section

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Cohort/ReviewViewModel.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Cohort/ReviewViewModel.cs
@@ -5,6 +5,7 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.Models.Cohort
     public class ReviewViewModel
     {
         public string AccountHashedId { get; set; }
+        public ApprenticeshipRequestsHeaderViewModel ApprenticeshipRequestsHeaderViewModel { get; set; }
         public IEnumerable<ReviewCohortSummaryViewModel> Cohorts { get; set; }
     }
 }
