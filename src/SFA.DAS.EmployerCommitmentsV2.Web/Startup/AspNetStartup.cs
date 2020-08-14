85701b53d7301610f278e95a91b61a614aa022ab
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2020-08-14 15:08:47 +0100

    CON-2210-rename

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Startup/AspNetStartup.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Startup/AspNetStartup.cs
@@ -32,7 +32,7 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.Startup
                 .AddUrlHelper()
                 .AddEmployerUrlHelper()
                 .AddMemoryCache()
-                .SaveKeysToStackExchangeRedis(_configuration, Environment);
+                .AddDataProtection(_configuration, Environment);
         }
 
         public void ConfigureContainer(Registry registry)
