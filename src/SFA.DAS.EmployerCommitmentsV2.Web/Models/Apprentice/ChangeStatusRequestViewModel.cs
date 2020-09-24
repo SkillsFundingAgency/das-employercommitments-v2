923c714309a67f7b4aef571cead1cf46de5f9d6e
Shahzad <hkf-tech@users.noreply.github.com>
2020-09-24 15:59:51 +0100

    CON-2419

---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Apprentice/ChangeStatusRequestViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Authorization.ModelBinding;
+using SFA.DAS.CommitmentsV2.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.Models.Apprentice
+{
+    public class ChangeStatusRequestViewModel : IAuthorizationContextModel
+    {
+        public ChangeStatusRequestViewModel()
+        {
+        }
+
+        public string AccountHashedId { get; set; }
+
+        public string ApprenticeshipHashedId { get; set; }
+
+        public long ApprenticeshipId { get; set; }
+
+        public ChangeStatusType? SelectedStatusChange { get; set; }
+    }
+
+    public enum ChangeStatusType
+    {
+        Stop,
+        Pause,
+        GoBack
+    }
+}
---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Apprentice/PauseRequestViewModel.cs
@@ -0,0 +1,28 @@
+﻿using SFA.DAS.Authorization.ModelBinding;
+using System;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.Models.Apprentice
+{
+    public class PauseRequestViewModel : IAuthorizationContextModel
+    {
+        public PauseRequestViewModel()
+        {
+        }
+
+        public string AccountHashedId { get; set; }
+
+        public string ApprenticeshipHashedId { get; set; }
+
+        public long ApprenticeshipId { get; set; }
+
+        public string ApprenticeName { get; set; }
+
+        public string ULN { get; set; }
+
+        public string Course { get; set; }
+
+        public DateTime PauseDate { get; set; }
+
+        public bool? PauseConfirmed { get; set; }
+    }
+}
