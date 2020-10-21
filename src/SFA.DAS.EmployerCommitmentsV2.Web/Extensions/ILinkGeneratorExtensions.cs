e9202317ab316ee6ce65fb9dca77c26a16aaea06
Ben Stone <ben1.stone@education.gov.uk>
2020-10-21 09:30:59 +0100

    CON-2502 Removes duplicate link generator extension method

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Extensions/ILinkGeneratorExtensions.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Extensions/ILinkGeneratorExtensions.cs
@@ -4,11 +4,7 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.Extensions
 {
     public static class ILinkGeneratorExtensions
     {
-        public static string AccountHome(this ILinkGenerator linkGenerator, string accountHashedId)
-        {
-            return linkGenerator.AccountsLink($"accounts/{accountHashedId}/teams");
-        }
-        public static string YourOrganisationsAndAgreements(this ILinkGenerator linkGenerator, string accountHashedId)
+       public static string YourOrganisationsAndAgreements(this ILinkGenerator linkGenerator, string accountHashedId)
         {
             return linkGenerator.AccountsLink($"accounts/{accountHashedId}/agreements");
         }
