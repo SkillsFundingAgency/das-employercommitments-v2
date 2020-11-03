44064eaf50e3618bafb0e2c375737e3b30471418
Ben Stone <ben1.stone@education.gov.uk>
2020-11-03 15:27:46 +0000

    CON-2507 Add validator and AuthorizationContext

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Mappers/Apprentice/ChangeProviderRequestedConfirmationViewModelMapper.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Mappers/Apprentice/ChangeProviderRequestedConfirmationViewModelMapper.cs
@@ -1,7 +1,6 @@
 ﻿using SFA.DAS.CommitmentsV2.Api.Client;
 using SFA.DAS.CommitmentsV2.Shared.Interfaces;
 using SFA.DAS.EmployerCommitmentsV2.Web.Models.Apprentice;
-using SFA.DAS.Encoding;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.EmployerCommitmentsV2.Web.Mappers.Apprentice
@@ -9,27 +8,22 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.Mappers.Apprentice
     public class ChangeProviderRequestedConfirmationViewModelMapper : IMapper<ChangeProviderRequestedConfirmationRequest, ChangeProviderRequestedConfirmationViewModel>
     {
         private readonly ICommitmentsApiClient _client;
-        private readonly IEncodingService _encodingService;
 
-        public ChangeProviderRequestedConfirmationViewModelMapper(ICommitmentsApiClient client, IEncodingService encodingService)
+        public ChangeProviderRequestedConfirmationViewModelMapper(ICommitmentsApiClient client)
         {
             _client = client;
-            _encodingService = encodingService;
         }
 
         public async Task<ChangeProviderRequestedConfirmationViewModel> Map(ChangeProviderRequestedConfirmationRequest source)
         {
-            
-            var apprenticeshipId = _encodingService.Decode(source.ApprenticeshipHashedId, EncodingType.ApprenticeshipId);
-            
             var providerResponse = await _client.GetProvider(source.ProviderId);
-            var apprenticeshipResponse = await _client.GetApprenticeship(apprenticeshipId);
+            var apprenticeshipResponse = await _client.GetApprenticeship(source.ApprenticeshipId);
 
             var result = new ChangeProviderRequestedConfirmationViewModel
             {
                 ApprenticeshipHashedId = source.ApprenticeshipHashedId,
                 AccountHashedId = source.AccountHashedId, 
-                Provider = providerResponse,
+                ProviderName = providerResponse.Name,
                 ApprenticeName = $"{apprenticeshipResponse.FirstName} {apprenticeshipResponse.LastName}"
             };
             
--- src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Apprentice/ChangeProviderRequestedConfirmationViewModel.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Apprentice/ChangeProviderRequestedConfirmationViewModel.cs
@@ -1,17 +1,14 @@
-﻿using SFA.DAS.CommitmentsV2.Api.Types.Responses;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using SFA.DAS.Authorization.ModelBinding;
 
 namespace SFA.DAS.EmployerCommitmentsV2.Web.Models.Apprentice
 {
-    public class ChangeProviderRequestedConfirmationViewModel
+    public class ChangeProviderRequestedConfirmationViewModel : IAuthorizationContextModel
     {
         public string AccountHashedId { get; set; }
         public string ApprenticeshipHashedId { get; set; }
+        public long ApprenticeshipId { get; set; }
 
-        public GetProviderResponse Provider { get; set; }
+        public string ProviderName { get; set; }
         public string ApprenticeName { get; set; }
     }
 }
