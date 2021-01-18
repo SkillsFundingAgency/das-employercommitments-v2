e660673022b1bec181a3a97b4fd866340b4f49e6
Ben Stone <ben1.stone@education.gov.uk>
2021-01-18 15:20:20 +0000

    CON-2521 Apply base viewmodel to EnterNewTrainingProviderViewModel

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Apprentice/EnterNewTrainingProviderViewModel.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Apprentice/EnterNewTrainingProviderViewModel.cs
@@ -1,27 +1,11 @@
 ﻿using SFA.DAS.CommitmentsV2.Api.Types.Responses;
-using SFA.DAS.Authorization.ModelBinding;
 using System.Collections.Generic;
 
 namespace SFA.DAS.EmployerCommitmentsV2.Web.Models.Apprentice
 {
-    public class EnterNewTrainingProviderViewModel : IAuthorizationContextModel
+    public class EnterNewTrainingProviderViewModel : ChangeOfProviderBaseViewModel
     {
         public long CurrentProviderId { get; set; }
         public List<Provider> Providers { get; set; }
-
-        // Move to base viewModel - will validators still work?
-        public string AccountHashedId { get; set; }
-        public string ApprenticeshipHashedId { get; set; }
-
-        public long ProviderId { get; set; }
-        public string ProviderName { get; set; }
-        public bool? EmployerWillAdd { get; set; }
-        public int? NewStartMonth { get; set; }
-        public int? NewStartYear { get; set; }
-        public int? NewEndMonth { get; set; }
-        public int? NewEndYear { get; set; }
-        public int? NewPrice { get; set; }
-
-        public bool Edit { get; set; }
     }
 }
