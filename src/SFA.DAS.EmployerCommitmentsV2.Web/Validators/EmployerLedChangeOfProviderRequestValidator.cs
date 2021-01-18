427a27ccaadda773037b3552750249bab4a81349
Ben Stone <ben1.stone@education.gov.uk>
2021-01-18 15:05:21 +0000

    CON-2519 Add EmployerLedChangeOfProviderRequestValidator + Tests

---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Validators/EmployerLedChangeOfProviderRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using SFA.DAS.EmployerCommitmentsV2.Web.Models.Apprentice;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.Validators
+{
+    public class EmployerLedChangeOfProviderRequestValidator : AbstractValidator<EmployerLedChangeOfProviderRequest>
+    {
+        public EmployerLedChangeOfProviderRequestValidator()
+        {
+            RuleFor(x => x.AccountHashedId).NotEmpty();
+            RuleFor(x => x.ApprenticeshipHashedId).NotEmpty();
+
+            RuleFor(x => x.ProviderId).GreaterThan(0);
+        }
+    }
+}
