7961803cb5e5defa224dffd1d9a360c6069cdfdc
Chris Woodcock <chris@humesoftware.com>
2021-02-24 17:16:06 +0000

    Added controller tests for payment priority

---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web.UnitTests/Controllers/EmployerManageApprenticesController/WhenCallingGetPaymentOrder.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Shared.Interfaces;
+using SFA.DAS.EmployerCommitmentsV2.Web.Controllers;
+using SFA.DAS.EmployerCommitmentsV2.Web.Models.EmployerManageApprentices;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Controllers.EmployerManageApprenticesControllerTests
+
+{
+    public class WhenCallingGetPaymentOrder
+    {
+        [Test, MoqAutoData]
+        public async Task Then_Returns_View_With_Correct_Model(
+            PaymentOrderRequest request,
+            PaymentOrderViewModel viewModel,
+            [Frozen] Mock<IModelMapper> mockMapper,
+            EmployerManageApprenticesController controller)
+        {
+            mockMapper
+                .Setup(mapper => mapper.Map<PaymentOrderViewModel>(request))
+                .ReturnsAsync(viewModel);
+
+            var result = await controller.PaymentOrder(request) as ViewResult;
+
+            result.ViewName.Should().BeNull();
+            var model = result.Model as PaymentOrderViewModel;
+            model.Should().BeSameAs(viewModel);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web.UnitTests/Controllers/EmployerManageApprenticesController/WhenCallingPostPaymentOrder.cs
@@ -0,0 +1,96 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Api.Client;
+using SFA.DAS.CommitmentsV2.Api.Types.Requests;
+using SFA.DAS.CommitmentsV2.Types;
+using SFA.DAS.EmployerCommitmentsV2.Web.Authentication;
+using SFA.DAS.EmployerCommitmentsV2.Web.Controllers;
+using SFA.DAS.EmployerCommitmentsV2.Web.Models.EmployerManageApprentices;
+using SFA.DAS.Testing.AutoFixture;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Controllers.EmployerManageApprenticesControllerTests
+{
+    public class WhenCallingPostPaymentOrder
+    {
+        [Test, MoqAutoData]
+        public async Task And_UpdateProviderPaymentsPriority_Succeeds_Then_Redirect_To_Home(
+            Mock<IAuthenticationService> mockAuthenticationService,
+            Mock<ICommitmentsApiClient> mockCommitmentsApiClient,
+            EmployerManageApprenticesController controller)
+        {
+            PaymentOrderViewModel viewModel = new PaymentOrderViewModel
+            {
+                ProviderPaymentOrder = new string[]
+                {
+                    "12345678",
+                    "23456789",
+                    "34567890"
+                }
+            };
+
+            mockCommitmentsApiClient
+                .Setup(r => r.UpdateProviderPaymentsPriority(
+                    It.IsAny<long>(),
+                    It.IsAny<UpdateProviderPaymentsPriorityRequest>(),
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.CompletedTask);
+
+            mockAuthenticationService
+                .SetupGet(p => p.UserInfo)
+                .Returns(new UserInfo
+                {
+                    UserId = Guid.NewGuid().ToString(),
+                    UserDisplayName = "Test, Tester",
+                    UserEmail = "tester@test.com"
+                });
+
+            var expectedRouteValues = new RouteValueDictionary(new
+            {
+                viewModel.AccountHashedId
+            });
+
+            var result = (await controller.PaymentOrder(mockAuthenticationService.Object, viewModel)) as RedirectToActionResult;
+
+            result.Should().NotBeNull();
+            result.ActionName.Should().Be("Index");
+            result.ControllerName.Should().Be("Home");
+            result.RouteValues.Should().BeEquivalentTo(expectedRouteValues);
+        }
+
+        [Test, MoqAutoData]
+        public async Task And_UpdateProviderPaymentsPriority_Fails_Then_Redirect_To_Error(
+            Mock<IAuthenticationService> mockAuthenticationService,
+            EmployerManageApprenticesController controller)
+        {
+            PaymentOrderViewModel viewModel = new PaymentOrderViewModel
+            {
+                ProviderPaymentOrder = new string[]
+                {
+                    "not a number"
+                }
+            };
+
+            mockAuthenticationService
+                .SetupGet(p => p.UserInfo)
+                .Returns(new UserInfo
+                {
+                    UserId = Guid.NewGuid().ToString(),
+                    UserDisplayName = "Test, Tester",
+                    UserEmail = "tester@test.com"
+                });
+
+            var result = (await controller.PaymentOrder(mockAuthenticationService.Object, viewModel)) as RedirectToActionResult;
+
+            result.Should().NotBeNull();
+            result.ActionName.Should().Be("Error");
+            result.ControllerName.Should().Be("Error");
+            result.RouteValues.Should().BeNull();
+        }
+    }
+}
