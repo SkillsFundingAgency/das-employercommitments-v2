662307d8accdd2e86aa6980d64a1a0f7a89d5de6
Shahzad <hkf-tech@users.noreply.github.com>
2021-04-14 15:10:30 +0100

    removed not required files

--- src/SFA.DAS.EmployerCommitmentsV2.Web/LocalDevRegistry/ToRemove/EditApprenticeshipResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿//namespace SFA.DAS.CommitmentsV2.Application.Commands.EditApprenticeship
-//{
-//    public class EditApprenticeshipResponse
-//    {
-//        public long ApprenticeshipId { get; set; }
-//        public bool NeedReapproval { get; set; }
-//    }
-//}
