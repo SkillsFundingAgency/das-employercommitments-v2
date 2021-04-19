e65f9d3919734e9f5af5e1dd0aac91ee10a6610f
Paul Graham <paul.graham@coprime.co.uk>
2021-04-19 10:56:47 +0100

    aded email address to view draft apprentice page

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Mappers/DraftApprenticeship/ViewDraftApprenticeshipViewModelMapper.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Mappers/DraftApprenticeship/ViewDraftApprenticeshipViewModelMapper.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading.Tasks;
+using SFA.DAS.Authorization.Services;
 using SFA.DAS.CommitmentsV2.Shared.Interfaces;
 using SFA.DAS.CommitmentsV2.Api.Client;
+using SFA.DAS.EmployerCommitmentsV2.Features;
 using SFA.DAS.EmployerCommitmentsV2.Web.Models.DraftApprenticeship;
 
 namespace SFA.DAS.EmployerCommitmentsV2.Web.Mappers.DraftApprenticeship
@@ -8,10 +10,12 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.Mappers.DraftApprenticeship
     public class ViewDraftApprenticeshipViewModelMapper : IMapper<ViewDraftApprenticeshipRequest, IDraftApprenticeshipViewModel>
     {
         private readonly ICommitmentsApiClient _commitmentsApiClient;
+        private readonly IAuthorizationService _authorizationService;
 
-        public ViewDraftApprenticeshipViewModelMapper(ICommitmentsApiClient commitmentsApiClient)
+        public ViewDraftApprenticeshipViewModelMapper(ICommitmentsApiClient commitmentsApiClient, IAuthorizationService authorizationService)
         {
             _commitmentsApiClient = commitmentsApiClient;
+            _authorizationService = authorizationService;
         }
 
         public async Task<IDraftApprenticeshipViewModel> Map(ViewDraftApprenticeshipRequest source)
@@ -35,10 +39,12 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.Mappers.DraftApprenticeship
                 StartDate = draftApprenticeship.StartDate,
                 EndDate = draftApprenticeship.EndDate,
                 Reference = draftApprenticeship.Reference,
-                LegalEntityName = source.Cohort.LegalEntityName
+                LegalEntityName = source.Cohort.LegalEntityName,
+                ShowEmail = await _authorizationService.IsAuthorizedAsync(EmployerFeature.ApprenticeEmail)
+
             };
 
-            return await Task.FromResult(result);
+            return result;
         }
     }
 }
