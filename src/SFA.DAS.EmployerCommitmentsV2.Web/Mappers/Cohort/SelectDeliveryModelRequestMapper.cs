37a5b22498c94b1281ad31a92ddd8cea25e70c9c
Alasdair Reid <ali.reid@live.co.uk>
2022-06-01 08:43:19 +0100

    Determine logic for radio options for use
    TODO determine logic for portable option

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Mappers/Cohort/SelectDeliveryModelRequestMapper.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Mappers/Cohort/SelectDeliveryModelRequestMapper.cs
@@ -9,32 +9,53 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.Mappers.Cohort
     public class SelectDeliveryModelRequestMapper : IMapper<SelectDeliveryModelRequest, SelectDeliveryModelViewModel>
     {
         private readonly IFjaaAgencyService _iFjaaAgencyService;
+        protected List<DeliveryModelMapped> _models;
 
         public SelectDeliveryModelRequestMapper(IFjaaAgencyService iFjaaAgencyService)
         {
             _iFjaaAgencyService = iFjaaAgencyService;
+            _models = new List<DeliveryModelMapped>();
         }
 
-        public Task<SelectDeliveryModelViewModel> Map(SelectDeliveryModelRequest source)
+        public async Task<SelectDeliveryModelViewModel> Map(SelectDeliveryModelRequest source)
         {
-            return Task.FromResult(new SelectDeliveryModelViewModel
+            bool agencyExists = await _iFjaaAgencyService.AgencyExists((int)source.AccountLegalEntityId);
+
+            // TODO determine logic for this
+            bool portableAllowed = true;
+
+            // Add regular model
+            this.AddDeliveryModel(0);
+
+            //Employer is listed on the RoFJAA & is not involved in Portable flexi-job pilot = Regular & Flexi - Job Agency delivery models available for selection
+            if (agencyExists && portableAllowed == false) { this.AddDeliveryModel(2); }
+
+            //Employer is listed on the RoFJAA & is also involved in Portable flexi - job pilot = Regular & Flexi - Job Agency delivery models available for selection
+            if (agencyExists && portableAllowed == true) { this.AddDeliveryModel(1); this.AddDeliveryModel(2);  }
+
+            //Employer is not listed on the RoFJAA & is involved in Portable flexi - job pilot = Regular & Portable Flexi - Job delivery models available for selection
+            if (!agencyExists && portableAllowed == true) { this.AddDeliveryModel(1);  }
+
+            return new SelectDeliveryModelViewModel
             {
                 AccountHashedId = source.AccountHashedId,
                 AccountLegalEntityHashedId = source.AccountLegalEntityHashedId,
                 CourseCode = source.CourseCode,
                 ReservationId = source.ReservationId,
                 StartMonthYear = source.StartMonthYear,
-                DeliveryModels = this.Models()
-            });
+                DeliveryModels = _models
+            };
         }
 
-        protected List<DeliveryModelMapped> Models()
+        protected void AddDeliveryModel(int deliveryModel)
         {
-            List<DeliveryModelMapped> models = new List<DeliveryModelMapped>();
-            models.Add(new DeliveryModelMapped() { Id = 0, Name = "Regular", Text = "The apprentice will have a single employment contract" });
-            models.Add(new DeliveryModelMapped() { Id = 1, Name = "Portable flexi-job", Text = "The apprentice will move between multiple employment contracts" });
-            models.Add(new DeliveryModelMapped() { Id = 2, Name = "Flexi-job agency", Text = "The apprentice will have a single employment contract with their flexi-job apprenticeship agency as they move between host businesses" });
-            return models;
+            if (deliveryModel == 0) { _models.Add(new DeliveryModelMapped() { Id = 0, Name = "Regular", Text = "The apprentice will have a single employment contract" }); }
+            if (deliveryModel == 1) { _models.Add(new DeliveryModelMapped() { Id = 1, Name = "Portable flexi-job", Text = "The apprentice will move between multiple employment contracts" }); }
+            if (deliveryModel == 2)
+            {
+                _models.Add(new DeliveryModelMapped() { Id = 2, Name = "Flexi-job agency", Text = "The apprentice will have a single employment contract with their flexi-job apprenticeship agency as they move between host businesses" });
+            }
         }
+
     }
 }
