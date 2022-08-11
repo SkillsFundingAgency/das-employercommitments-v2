60b941c6a2dd8634d4fddcb5fc64a4c19ecfe0d1
chazzysb <cballagan@yahoo.com>
2022-08-11 13:09:05 +0100

    PA-463-RetainProviderId

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Cohort/DetailsViewModel.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Cohort/DetailsViewModel.cs
@@ -15,6 +15,7 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.Models.Cohort
         public long CohortId { get; set; }
         public string AccountLegalEntityHashedId { get; set; }
         public string LegalEntityName { get; set; }
+        public long? ProviderId { get; set; }
         public string ProviderName { get; set; }
         public string Message { get; set; }
         public string TransferSenderHashedId { get; set; }
