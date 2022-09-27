56f88ece3861216c3ff3f8f6bea87dfcf9a93da5
Michael Young <mick.young1981@googlemail.com>
2022-09-27 09:44:14 +0100

    Fix validation

---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Filters/AjaxValidationAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using SFA.DAS.CommitmentsV2.Api.Types.Validation;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.Filters
+{
+    public class AjaxValidationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var controller = (Controller) filterContext.Controller;
+            if (!controller.ModelState.IsValid)
+            {
+                var errors = FindErrors(controller.ModelState);
+                if (errors.Any())
+                {
+                    // Short circuit with a JSON response
+                    filterContext.HttpContext.Response.StatusCode = (int)HttpStatusCode.OK;
+                    filterContext.Result = new JsonResult(errors);
+                }
+            }
+            base.OnActionExecuting(filterContext);
+        }
+
+        internal static IEnumerable<ErrorDetail> FindErrors(ModelStateDictionary modelState)
+        {
+            var result = new List<ErrorDetail>();
+            var erroneousFields = modelState.Where(ms => ms.Value.Errors.Any()).Select(x => new { x.Key, x.Value.Errors });
+            foreach (var erroneousField in erroneousFields)
+            {
+                var fieldKey = erroneousField.Key;
+                var fieldErrors = erroneousField.Errors.Select(error => new ErrorDetail(fieldKey, error.ErrorMessage));
+                result.AddRange(fieldErrors);
+            }
+            return result;
+        }
+    }
+}
