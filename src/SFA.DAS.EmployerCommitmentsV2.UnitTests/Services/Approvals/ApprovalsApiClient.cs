97b719b4a5aa5bbe0f0cd2981d07b060294aa717
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-11-16 20:33:36 +0000

    FAI-627 - Add request response for getting employer user accounts

---
+++ src/SFA.DAS.EmployerCommitmentsV2.UnitTests/Services/Approvals/ApprovalsApiClient.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using System.Web;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerCommitmentsV2.Services.Approvals;
+using SFA.DAS.EmployerCommitmentsV2.Services.Approvals.Responses;
+
+namespace SFA.DAS.EmployerCommitmentsV2.UnitTests.Services.Approvals
+{
+    
+    public class ApprovalsApiClientTests
+    {
+        [Test, AutoData]
+        public async Task Then_The_Data_Is_Returned_From_The_Encoded_Url(string email, string userId, GetUserAccountsResponse response)
+        {
+            email = $"{email}@email.com";
+            var outerApiClient = new Mock<IOuterApiClient>();
+            var expectedUrl = $"AccountUsers/{userId}/accounts?{HttpUtility.UrlEncode(email)}";
+            outerApiClient.Setup(x => x.Post<GetUserAccountsResponse>(expectedUrl, null)).ReturnsAsync(response);
+            var approvalsApiClient = new ApprovalsApiClient(outerApiClient.Object);
+        
+            var actual = await approvalsApiClient.GetEmployerUserAccounts(email, userId);
+        
+            actual.Should().BeEquivalentTo(response);
+        }
+    }
+}
