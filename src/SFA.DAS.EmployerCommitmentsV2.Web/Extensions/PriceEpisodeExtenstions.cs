13d6dd3a8aa73a17d08e69d3191fa390e3cc5523
Alasdair Reid <ali.reid@live.co.uk>
2023-08-16 09:55:24 +0100

    Tests

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Extensions/PriceEpisodeExtenstions.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Extensions/PriceEpisodeExtenstions.cs
@@ -37,7 +37,7 @@ namespace SFA.DAS.EmployerCommitmentsV2.Web.Extensions
             var episode = episodes.FirstOrDefault(x =>
                 x.FromDate <= effectiveDate && (x.ToDate == null || x.ToDate >= effectiveDate));
 
-            return (int)(episode?.Cost ?? episodes.FirstOrDefault().Cost);
+            return (int)(episode?.Cost ?? episodes.First().Cost);
         }
 
     }
