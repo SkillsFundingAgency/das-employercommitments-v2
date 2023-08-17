009c8d5a450bba3974b8d7b73e7fbdf89fda1868
Jon Harris <jon.harris@digital.education.gov.uk>
2023-08-17 07:53:29 +0100

    Added allow anonymous to error controller

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Controllers/ErrorController.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Controllers/ErrorController.cs
@@ -1,8 +1,10 @@
 using System.Net;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SFA.DAS.EmployerCommitmentsV2.Web.Controllers
 {
+    [AllowAnonymous]
     public class ErrorController : Controller
     {
         [Route("badrequest")]
