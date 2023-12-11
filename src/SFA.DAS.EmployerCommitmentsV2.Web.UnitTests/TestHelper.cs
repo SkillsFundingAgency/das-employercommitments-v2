2e2159a3bc9de81ecd80bd35cdd59721fe7ebee9
Jon Harris <jon.harris@digital.education.gov.uk>
2023-12-11 12:52:21 +0000

    Tidy

--- src/SFA.DAS.EmployerCommitmentsV2.Web.UnitTests/TestHelper.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web.UnitTests/TestHelper.cs
@@ -1,20 +1,12 @@
-﻿using System.Text;
-using KellermanSoftware.CompareNetObjects;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests;
 
-public class TestHelper
+public static class TestHelper
 {
     public static T Clone<T>(T source)
     {
         var serialized = JsonConvert.SerializeObject(source);
         return JsonConvert.DeserializeObject<T>(serialized);
-    }
-
-    public static bool EnumerablesAreEqual(IEnumerable<object> expected, IEnumerable<object> actual)
-    {
-        return new CompareLogic(new ComparisonConfig { IgnoreObjectTypes = true })
-            .Compare(expected, actual).AreEqual;
     }
 }
