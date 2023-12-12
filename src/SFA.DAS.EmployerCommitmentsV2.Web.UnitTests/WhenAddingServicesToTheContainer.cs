3874a80abd066074bf88dca6bb6c87c74cf3b90d
Jon Harris <jon.harris@digital.education.gov.uk>
2023-12-12 09:41:15 +0000

    Tidy

--- src/SFA.DAS.EmployerCommitmentsV2.Web.UnitTests/WhenAddingServicesToTheContainer.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web.UnitTests/WhenAddingServicesToTheContainer.cs
@@ -110,8 +110,8 @@ public class WhenAddingServicesToTheContainer
         services.AddModelMappings();
         services.AddApplicationServices(employerCommitmentsV2Configuration);
         services.AddModelMappings();
-        services.AddDasMvc(configuration)
-            .AddEmployerUrlHelper();
+        services.AddDasMvc(configuration);
+        services.AddEmployerUrlHelper();
 
         services.AddTransient<IUrlHelper, StubUrlHelper>();
 
