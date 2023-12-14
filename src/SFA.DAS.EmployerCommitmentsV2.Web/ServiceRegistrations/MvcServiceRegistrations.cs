772e8dedee39d464c57514ba1d087a345bb70d8b
Jon Harris <jon.harris@digital.education.gov.uk>
2023-12-14 15:43:27 +0000

    Auth tweaks

--- src/SFA.DAS.EmployerCommitmentsV2.Web/ServiceRegistrations/MvcServiceRegistrations.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/ServiceRegistrations/MvcServiceRegistrations.cs
@@ -34,12 +34,6 @@ public static class MvcServiceRegistrations
                 options.ModelBinderProviders.Insert(0, new AuthorizationModelBinderProvider());
                 options.ModelBinderProviders.Insert(1, new SuppressArgumentExceptionModelBinderProvider());
                 options.AddStringModelBinderProvider();
-                //options.Filters.Add(new AccountActiveFilter(configuration));
-
-                if (commitmentsConfiguration.UseGovSignIn)
-                {
-                    options.Filters.Add(new AuthorizeFilter(PolicyNames.HasActiveAccount));
-                }
             })
             .AddControllersAsServices()
             .SetDefaultNavigationSection(NavigationSection.ApprenticesHome);
