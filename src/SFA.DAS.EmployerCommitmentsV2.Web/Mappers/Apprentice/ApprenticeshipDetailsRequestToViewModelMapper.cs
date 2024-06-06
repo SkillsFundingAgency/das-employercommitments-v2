4e45bbdb704aed6bfc4d39e70cc2a70271fb8ab4
tom-gough <tom.gough.tgsd@outlook.com>
2024-06-06 08:04:33 +0100

    FLP-699 Updated api response to match new schema

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Mappers/Apprentice/ApprenticeshipDetailsRequestToViewModelMapper.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Mappers/Apprentice/ApprenticeshipDetailsRequestToViewModelMapper.cs
@@ -126,7 +126,7 @@ public class ApprenticeshipDetailsRequestToViewModelMapper : IMapper<Apprentices
                 PendingPriceChangeUrl = response.PendingPriceChange != null ? _urlBuilder.ApprenticeshipsLink("ViewPendingPriceChange", source.AccountHashedId, source.ApprenticeshipHashedId) : null,
                 PendingStartDateChange = Map(response.PendingStartDateChange),
                 PendingStartDateChangeUrl = response.PendingStartDateChange != null ? _urlBuilder.ApprenticeshipsLink("ViewPendingStartDateChange", source.AccountHashedId, source.ApprenticeshipHashedId) : null,
-                PaymentStatus = response.PaymentsFrozen == true ? "Inactive" : "Active"
+                PaymentStatus = response.PaymentsStatus.PaymentsFrozen ? "Inactive" : "Active"
             };
 		
             return result;
