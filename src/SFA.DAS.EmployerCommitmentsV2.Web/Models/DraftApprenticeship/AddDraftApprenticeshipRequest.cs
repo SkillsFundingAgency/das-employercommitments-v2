e9eb8a0d2bb5e805ec48c2f7a42579f4b0ade171
Paul Graham <paul.graham@coprime.co.uk>
2024-09-10 15:59:07 +0100

    removed AutoCreated as it wasn't ever being used

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Models/DraftApprenticeship/AddDraftApprenticeshipRequest.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Models/DraftApprenticeship/AddDraftApprenticeshipRequest.cs
@@ -19,7 +19,6 @@ public class BaseAddDraftApprenticeshipRequest
     public Guid? ReservationId { get; set; }
     public string StartMonthYear { get; set; }
     public string CourseCode { get; set; }
-    public bool AutoCreated { get; set; }
     public long ProviderId { get; set; }
     public DeliveryModel? DeliveryModel { get; set; }
     public int? Cost { get; set; }
