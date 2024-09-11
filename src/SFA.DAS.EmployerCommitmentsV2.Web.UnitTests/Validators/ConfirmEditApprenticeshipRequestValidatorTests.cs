ec310f2f4c98c3cd10ed920fccaa31e629df6743
ogrwill <will.ogrady@digital.education.gov.uk>
2024-09-11 14:28:22 +0100

    validator tests

---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web.UnitTests/Validators/ConfirmEditApprenticeshipRequestValidatorTests.cs
@@ -0,0 +1,17 @@
+﻿using SFA.DAS.EmployerCommitmentsV2.Web.Models.Apprentice;
+using SFA.DAS.EmployerCommitmentsV2.Web.Validators;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Validators;
+
+[TestFixture]
+public class ConfirmEditApprenticeshipRequestValidatorTests : ValidatorTestBase<ConfirmEditApprenticeshipRequest, ConfirmEditApprenticeshipRequestValidator>
+{
+    [TestCase("00000000-0000-0000-0000-000000000000", false)]
+    [TestCase("a7846ec9-7e58-4ccd-98b9-9803dc8bae6d", true)]
+    public void ThenValidatesCacheKey(string cacheKey, bool expectedValid)
+    {
+        var request = new ConfirmEditApprenticeshipRequest { CacheKey = Guid.Parse(cacheKey) };
+
+        AssertValidationResult(x => x.CacheKey, request, expectedValid);
+    }
+}
---
+++ src/SFA.DAS.EmployerCommitmentsV2.Web.UnitTests/Validators/EditApprenticeshipRequestValidatorTests.cs
@@ -0,0 +1,17 @@
+﻿using SFA.DAS.EmployerCommitmentsV2.Web.Models.Apprentice;
+using SFA.DAS.EmployerCommitmentsV2.Web.Validators;
+
+namespace SFA.DAS.EmployerCommitmentsV2.Web.UnitTests.Validators;
+
+[TestFixture]
+public class EditApprenticeshipRequestValidatorTests : ValidatorTestBase<EditApprenticeshipRequest, EditApprenticeshipRequestValidator>
+{
+    [TestCase("00000000-0000-0000-0000-000000000000", false)]
+    [TestCase("a7846ec9-7e58-4ccd-98b9-9803dc8bae6d", true)]
+    public void ThenValidatesCacheKey(string cacheKey, bool expectedValid)
+    {
+        var request = new EditApprenticeshipRequest { CacheKey = Guid.Parse(cacheKey) };
+
+        AssertValidationResult(x => x.CacheKey, request, expectedValid);
+    }
+}
