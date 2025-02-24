ad1a302b8095504fa3a4e08f2be219e3f9d6afda
Jon Harris <jon.harris@fuitsu.com>
2025-02-24 14:42:55 +0000

    Aligning the SFA.DAS.Validation.Mvc package versions

--- src/SFA.DAS.EmployerCommitmentsV2.Web/ServiceRegistrations/MvcServiceRegistrations.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/ServiceRegistrations/MvcServiceRegistrations.cs
@@ -1,11 +1,7 @@
 ﻿using FluentValidation.AspNetCore;
-using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Microsoft.Extensions.Configuration;
 using SFA.DAS.CommitmentsV2.Shared.Extensions;
 using SFA.DAS.Employer.Shared.UI;
-using SFA.DAS.EmployerCommitmentsV2.Configuration;
-using SFA.DAS.EmployerCommitmentsV2.Web.Authorization;
 using SFA.DAS.EmployerCommitmentsV2.Web.Filters;
 using SFA.DAS.EmployerCommitmentsV2.Web.ModelBinding;
 using SFA.DAS.EmployerCommitmentsV2.Web.Validators;
