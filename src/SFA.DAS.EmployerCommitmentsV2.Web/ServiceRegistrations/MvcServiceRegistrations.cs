14a96444b51cf3d107f10df3223e63de94cf8133
Jon Harris <jon.harris@fuitsu.com>
2025-03-11 10:16:24 +0000

    Tweaking validation registration

--- src/SFA.DAS.EmployerCommitmentsV2.Web/ServiceRegistrations/MvcServiceRegistrations.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/ServiceRegistrations/MvcServiceRegistrations.cs
@@ -1,10 +1,11 @@
 ﻿using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
-using SFA.DAS.CommitmentsV2.Shared.Extensions;
+using SFA.DAS.CommitmentsV2.Shared.Filters;
 using SFA.DAS.Employer.Shared.UI;
 using SFA.DAS.EmployerCommitmentsV2.Web.Filters;
 using SFA.DAS.EmployerCommitmentsV2.Web.ModelBinding;
 using SFA.DAS.EmployerCommitmentsV2.Web.Validators;
+using SFA.DAS.Validation.Mvc.Extensions;
 
 namespace SFA.DAS.EmployerCommitmentsV2.Web.ServiceRegistrations;
 
@@ -19,6 +20,7 @@ public static class MvcServiceRegistrations
             {
                 options.EnableEndpointRouting = false;
                 options.AddValidation();
+                options.Filters.Add<DomainExceptionRedirectGetFilterAttribute>();
                 options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
                 options.Filters.Add(new GoogleAnalyticsFilter());
                 options.ModelBinderProviders.Insert(0, new AuthorizationModelBinderProvider());
