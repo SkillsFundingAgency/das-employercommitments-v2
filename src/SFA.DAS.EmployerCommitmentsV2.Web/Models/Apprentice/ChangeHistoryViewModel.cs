6b61c6ad963d7132966d4f2986cc94ba93daafda
skumardfe <Santhosh.KUMAR@education.gov.uk>
2026-06-08 16:46:53 +0100

    add link and update the enum description

--- src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Apprentice/ChangeHistoryViewModel.cs
+++ src/SFA.DAS.EmployerCommitmentsV2.Web/Models/Apprentice/ChangeHistoryViewModel.cs
@@ -31,13 +31,13 @@ public enum LearningChangeType : byte
     [Description("Auto approved")]
     AutoApproved = 0,
 
-    [Description("Rejected")]
+    [Description("Declined")]
     Rejected = 1,
 
     [Description("Approved")]
     EmployerApproved = 2,
 
-    [Description("Rejected")]
+    [Description("Declined")]
     EmployerRejected = 3,
 
     [Description("Manual update")]
